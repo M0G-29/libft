4201be9f8c85accf992336c45921d7bc0095ae68
Luis <lp_santos29@outlook.com>
2021-05-19 17:17:17 -0300

    Fix strlen

---
+++ functions/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 14:01:58 by lpaulo-d          #+#    #+#             */
+/*   Updated: 2021/05/19 17:15:59 by lpaulo-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t		ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	len_src;
+	size_t	i;
+
+	len_src = 0;
+	while (src[len_src] != '\0')
+	{
+		len_src++;
+	}
+	if (size == 0)
+	{
+		return (len_src);
+	}
+	i = 0;
+	while (src[i] != '\0' && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (len_src);
+}
