c5037c49aa5fee209f5fbd8e50ac9142930917b6
Luis <lp_santos29@outlook.com>
2021-05-22 22:53:40 -0300

    Update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 20:47:31 by lpaulo-d          #+#    #+#             */
-/*   Updated: 2021/05/22 20:52:14 by lpaulo-d         ###   ########.fr       */
+/*   Updated: 2021/05/22 22:51:20 by lpaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*p;
-	size_t	i;
+	size_t			i;
 
 	i = 0;
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
 	while (p[i] < (nmemb * size))
 	{
 		p[i] = 0;
