3ac4b3c372deef0ebefb3e4e0892d81781c43904
luis <lp_santos29@outlook.com>
2021-05-28 19:24:41 -0300

    Update AND add functions part2

---
+++ ft_itoa.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/28 11:12:20 by lpaulo-d          #+#    #+#             */
+/*   Updated: 2021/05/28 19:24:00 by lpaulo-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	int			sign;
+	int			size;
+	char		*conv;
+	long int	nn;
+	
+	sign = n < 0;//operacao logica de comparacao,se for retorna 1 se n 0 (true or false), armazena em sign.
+	nn = n;//para n mexer diretamente no n, é um bckp
+	if (sign > 0)
+		nn = -nn;//tiro o sinal
+	size = 1;//pq o n tem o sinal de negativo entao meio q pularia e no while embaixo ele n pega o primeiro digito ele sai antes
+	while (n / 10 != 0 && size++)
+		n /= 10;//estou pegando q quantidade de caracteres, tirando numero por numero ate dar 0 e sair do while
+	conv = malloc(sizeof(char) * (size + sign + 1));//aqui aloco memoria para o tamanho de digitos e para o sinal negativo e o \0 no fim
+	if (!conv)
+		return (NULL);
+	conv[size + sign] = '\0';// coloco o \0 antes de decrementar pq vou fazer correr o caminho inverso <--
+	while (size-- != 0)
+	{
+		conv[size + sign] = nn % 10 + '0';
+		nn /= 10;
+	}
+	if (sign > 0)
+		conv[0] = '-';
+	return (conv);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/22 20:47:31 by lpaulo-d          #+#    #+#             */
-/*   Updated: 2021/05/28 11:10:48 by lpaulo-d         ###   ########.fr       */
+/*   Created: 2021/05/28 18:35:51 by lpaulo-d          #+#    #+#             */
+/*   Updated: 2021/05/28 19:20:23 by lpaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void ft_putstr_fd(char *s, int fd)
 {
-	void	*nn;
-
-	nn = malloc(nmemb * size);
-	if (nn == NULL)
-		return (NULL);
-	ft_bzero(nn, nmemb * size);
-	return (nn);
+	write(fd, s, ft_strlen(s));
 }
