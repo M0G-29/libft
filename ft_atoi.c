fcab7b2264c3d8dfca0684a70f35ad2a8bafb42d
luis <lp_santos29@outlook.com>
2021-06-01 16:42:34 -0300

    norminette check

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,39 @@
 /*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 13:12:04 by lpaulo-d          #+#    #+#             */
-/*   Updated: 2021/06/01 16:14:47 by lpaulo-d         ###   ########.fr       */
+/*   Updated: 2021/06/01 16:42:26 by lpaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_ignore(char c)
+static int	ft_isspace(char c)
 {
-	if ((c >= 9 && c <= 13) || c == ' ')
+	if (c == ' ' || c == '\n' || c == '\t' ||
+			c == '\v' || c == '\f' || c == '\r')
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
-int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *str)
 {
+	int	result;
 	int	sign;
-	int	number;
+	int	i;
 
+	i = 0;
+	result = 0;
 	sign = 1;
-	number = 0;
-	while (ft_ignore(*nptr) == 1)
-		nptr++;
-	if (*nptr == '-' || *nptr == '+')
-		if (*nptr++ == '-')
-			sign *= -1;
-	while (ft_isdigit(*nptr) == 1)
-		number = number * 10 + (*nptr++ - '0');
-	return (number * sign);
+	while (str[i] != '\0' && ft_isspace(str[i]) == 1)
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if ((str[i] == '-') || (str[i] == '+'))
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i++] - '0');
+	}
+	return (result * sign);
 }
